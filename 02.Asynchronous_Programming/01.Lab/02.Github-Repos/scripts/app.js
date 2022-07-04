b5890c1888a67f474994c5dfdc700b4529486376
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-07-04 20:24:12 +0300

    Solution with async/await

--- 02.Asynchronous_Programming/01.Lab/02.Github-Repos/scripts/app.js
+++ 02.Asynchronous_Programming/01.Lab/02.Github-Repos/scripts/app.js
@@ -1,33 +1,59 @@
-function loadRepos() {
+async function loadRepos() {
 	let username = document.getElementById('username').value;
 	let loadButtonElement = document.querySelector('button');
 	let reposList = document.getElementById('repos');
 
-	fetch(`https://api.github.com/users/${username}/repos`)
-		.then(handleResponse)
-		.then(displayRepos)
-		.catch(displayError);
+	// 1) with then/cath
 
-	function handleResponse(response) {
+	// fetch(`https://api.github.com/users/${username}/repos`)
+	// 	.then(handleResponse)
+	// 	.then(displayRepos)
+	// 	.catch(displayError);
+
+	// function handleResponse(response) {
+	// 	if (response.ok == false) {
+	// 		throw new Error(`${response.status} ${response.statusText}`);
+	// 	}
+	// 	return response.json();
+	// }
+
+	// function displayRepos(data) {
+	// 	reposList.innerHTML = '';
+	// 	for (let repo of data) {
+	// 		let li = document.createElement('li');
+	// 		let  href = document.createElement('a');
+	// 		href.href = repo.html_url;
+	// 		href.textContent = repo.full_name;
+	// 		li.appendChild(href)
+	// 		reposList.appendChild(li);
+	// 	}
+	// }
+
+	// function displayError(error) {
+	// 	reposList.innerHTML = `${error.message}`;
+	// }
+
+	// 2) with async/await
+
+	try {
+		let response = await fetch(`https://api.github.com/users/${username}/repos`);
+
 		if (response.ok == false) {
 			throw new Error(`${response.status} ${response.statusText}`);
 		}
-		return response.json();
-	}
-
-	function displayRepos(data) {
+		let data = await response.json();
 		reposList.innerHTML = '';
 		for (let repo of data) {
 			let li = document.createElement('li');
-			let  href = document.createElement('a');
+			let href = document.createElement('a');
 			href.href = repo.html_url;
 			href.textContent = repo.full_name;
 			li.appendChild(href)
 			reposList.appendChild(li);
 		}
-	}
 
-	function displayError(error) {
+	} catch (error) {
 		reposList.innerHTML = `${error.message}`;
+
 	}
 }
