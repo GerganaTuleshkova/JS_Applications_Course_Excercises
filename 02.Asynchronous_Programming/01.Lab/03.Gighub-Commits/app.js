de9d6ab8d449a61a679563eb0d10a443a7f3327e
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-07-04 21:05:31 +0300

    Problem 3 added

--- 02.Asynchronous_Programming/01.Lab/03.Gighub-Commits/app.js
+++ 02.Asynchronous_Programming/01.Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,32 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let username = document.getElementById('username').value;
+    let repoName = document.getElementById('repo').value;
+	let loadButtonElement = document.querySelector('button');
+	let commitsList = document.getElementById('commits');
+
+	fetch(`https://api.github.com/repos/${username}/${repoName}/commits`)
+		.then(handleResponse)
+		.then(displayCommits)
+		.catch(displayError);
+
+	function handleResponse(response) {
+		if (response.ok == false) {
+			throw new Error(`Error: ${response.status} (${response.statusText})`);
+		}
+		return response.json();
+	}
+
+	function displayCommits(data) {
+		commitsList.innerHTML = '';
+		for (let commit of data) {
+			let li = document.createElement('li');
+            li.textContent = `${commit.commit.author.name}: ${commit.commit.message} `
+			
+			commitsList.appendChild(li);
+		}
+	}
+
+	function displayError(error) {
+		commitsList.innerHTML = `${error.message}`;
+	}
 }
