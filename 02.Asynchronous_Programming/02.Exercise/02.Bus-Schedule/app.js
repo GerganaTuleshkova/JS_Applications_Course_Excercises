7f9b05e71d87b3dfccb4c825ef765f9242e59972
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-07-05 13:32:30 +0300

    Problem 2 added

--- 02.Asynchronous_Programming/02.Exercise/02.Bus-Schedule/app.js
+++ 02.Asynchronous_Programming/02.Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,37 @@
 function solve() {
+    let baseUrl = 'http://localhost:3030/jsonstore/bus/schedule/';
+    let infoElement = document.querySelector('.info');
+    let departButton = document.getElementById('depart');
+    let arriveButton = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+    let busStop  = {
+        next: 'depot',
+    }
+
+    async function depart() {
+        arriveButton.disabled = false;
+        departButton.disabled = true;
+        try {
+            let response = await fetch(`${baseUrl}${busStop.next}`);
+            if (response.ok == false) {
+                throw new Error(`Error: ${response.status} (${response.statusText})`);
+            }
+            let data = await response.json();
+            busStop = Object.assign(data);
+            infoElement.textContent = `Next stop ${busStop.name}`;
+        }
+        catch (error) {
+            infoElement.textContent = `Error`;
+            arriveButton.disabled = true;
+            departButton.disabled = true;
+            infoElement.style.backgroundColor = '#9C3107';
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        arriveButton.disabled = true;
+        departButton.disabled = false;
+        infoElement.textContent = `Arriving at ${busStop.name}`;
     }
 
     return {
@@ -15,3 +41,36 @@ function solve() {
 }
 
 let result = solve();
+
+// function solve() {
+//     const getRef = type => document.querySelector(type);
+//     const [infoRef, departRef, arriveRef] = [getRef('.info'), getRef('#depart'), getRef('#arrive')];
+//     const [url, infoDiv] = ['http://localhost:3030/jsonstore/bus/schedule/', getRef('#info')];
+//     let data = { next: 'depot' };
+ 
+//     async function depart() {
+//         try {
+//             data = await (await fetch(url + data.next)).json();
+//             infoRef.textContent = `Next stop ${data.name}`;
+//             changeStatus(departRef, arriveRef);
+//         } catch (error) {
+//             changeStatus(departRef, arriveRef, true);
+//             infoRef.textContent = 'Error';
+//             infoDiv.style.backgroundColor = '#9C3107';
+//         }
+//     }
+ 
+//     function arrive() {
+//         infoRef.textContent = `Arriving at ${data.name}`;
+//         changeStatus(departRef, arriveRef);
+//     }
+ 
+//     return { depart, arrive };
+// }
+ 
+// let result = solve();
+ 
+// function changeStatus(x, y, both) {
+//     x.disabled = both || !x.disabled;
+//     y.disabled = both || !y.disabled;
+// }
