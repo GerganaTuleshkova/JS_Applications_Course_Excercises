3e618e66c0060959bf80c23472fc1766ef7e4b2e
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-07-19 19:24:40 +0300

    Test added

--- 05.Architecture_and_Testing/02.Excercise/01.Messenger/test.js
+++ 05.Architecture_and_Testing/02.Excercise/01.Messenger/test.js
@@ -7,52 +7,39 @@ let browser, page; // Declare reusable variables
 describe('E2E tests', async function () {
     this.timeout(5000);
 
-    before(async () => { browser = await chromium.launch({ headless: false, slowMo: 1000 }); });
+    before(async () => { browser = await chromium.launch({ }); });
     after(async () => { await browser.close(); });
     beforeEach(async () => { page = await browser.newPage(); });
     afterEach(async () => { await page.close(); });
 
-    it('loads article titles', async () => {
+    it('loads messages in textarea', async () => {
         await page.goto('http://localhost:5500');
-        await page.click('#refresh');
-        await page.screenshot({path: 'site.png'});    
-        
-        await page.waitForSelector('#messages');
+        await page.click('text=Refresh');
+        // await page.screenshot({path: 'site.png'});     
+        // await page.waitForSelector('textarea');
 
-        const testMessages = await page.textContent('#messages');
-        console.log(testMessages)
+        let rowData = await page.$eval('textarea', rows => rows.value);
 
-        expect(testMessages).to.contain('Spami: Hello, are you there?');
-        // expect(content).to.contain('Open standard');
-        // expect(content).to.contain('Unix');
-        // expect(content).to.contain('ALGOL');
+        expect(rowData).to.contains('Spami: Hello, are you there?');       
     });
 
-    // it('loads article content when more is clicked', async () => {
-    //     await page.goto('http://localhost:5500');
-    //     await page.click('text=More');
+    it.only('creates message', async () => {
+        await page.goto('http://localhost:5500');
+        // await page.screenshot({path: 'site.png'});     
 
-    //     const visibleParagraph = await page.isVisible('.accordion p');
+        let testName = 'Test name';
+        let testMessage = 'Test message';
 
-    //     expect(visibleParagraph).to.be.true;
-    // });
+        await page.fill('#author', testName);
+        await page.fill('#content', testMessage);
+        let [request, _] = await Promise.all([
+            page.waitForRequest((request) => request.method() == 'POST'),
+            page.click('text=Send')
+        ])
 
-    // it('hide article content when Less is clicked', async () => {
-    //     await page.goto('http://localhost:5500');
+        let data = JSON.parse(request.postData());
 
-    //     await page.click('text=More');
-    //     await page.waitForSelector('.accordion p');
-
-    //     let visibleParagraph = await page.isVisible('.accordion p');
-    //     expect(visibleParagraph).to.be.true;
-
-    //     await page.click('text=Less');
-    //     notVisible = await page.isVisible('.accordion p');
-    //     expect(notVisible).to.be.false;
-    // });
-
-    // it.only('fills input form', async () => {
-    //     await page.goto('http://localhost:5500');
-    //     await page.fill('[name="email"]', 'peter@abv.bg');       
-    // });
+        expect(data.author).to.equal(testName);
+        expect(data.content).to.equal(testMessage);       
+    });    
 });
