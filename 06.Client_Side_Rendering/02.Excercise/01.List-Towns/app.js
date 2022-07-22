5c533b0daae20a089a097f83ceb8e6fb07b7a026
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-07-22 21:35:08 +0300

    Problem 1 added

---
+++ 06.Client_Side_Rendering/02.Excercise/01.List-Towns/app.js
@@ -0,0 +1,24 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+
+let cityTemplate = (citiesData) =>
+    html`<ul>
+    ${citiesData.map(c => html`<li>${c}</li>`)}
+    </ul>`;
+
+document.getElementById('btnLoadTowns').addEventListener('click', onclick)
+
+function onclick(event) {
+    event.preventDefault();
+
+    let container = document.getElementById('root');
+    // renderCities();
+
+    // function renderCities() {
+    //     let inputArray = document.getElementById('towns').value.split(', ')
+    //     render(inputArray.map(cityTemplate), container);
+    // }
+
+    let inputArray = document.getElementById('towns').value.split(', ')
+    render(cityTemplate(inputArray), container)
+
+}
